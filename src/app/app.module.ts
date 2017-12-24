022b93f85e273ab531af28f5d4d981c5865e268e
Casey Herold <caseynate@gmail.com>
2017-12-24 00:08:29 -0700

    Added http get request🏖

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CategoryComponent } from './category/category.component';
+import { CategoryService } from './category.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,10 @@ import { CategoryComponent } from './category/category.component';
     CategoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CategoryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get(`http://www.jservice.io/api/clues/?category=${Math.floor(Math.random() * 300)}`).subscribe(categoryQuestions => {
+      const categoryTitle = categoryQuestions[Math.floor(Math.random() * 40)].category.title
+      const value = categoryQuestions[1].value
+      console.log(value)
+    })
   }
 
 }
